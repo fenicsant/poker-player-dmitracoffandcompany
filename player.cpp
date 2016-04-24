984362a81ca29263d886c982054bed816a3d209d
fenicsant <fenicsant@gmail.com>
2016-04-24 11:11:16 +0400

    classes

--- player.cpp
+++ player.cpp
@@ -2,6 +2,36 @@
 
 const char* Player::VERSION = "Default C++ folding player";
 
+#include <list>
+
+const char* Player::VERSION = "Default C++ folding player";
+
+class Hand;
+
+
+class Card
+{
+public:
+    enum Suit { hearts, spades};
+    Suit suit;
+    int rank; // 2-2 .. 10-10 11-V 12-D 13-K 14-A
+};
+
+class Table
+{
+public :
+    Hand * self;
+    list<Hand*> player;
+    list<Card> common;
+};
+
+class Hand
+{
+   list<Card> cards;
+   int cost();
+};
+
+
 int Player::betRequest(json::Value game_state)
 {
     int bet = random();
